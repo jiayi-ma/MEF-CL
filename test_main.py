280e8682126beb74e0f4bf42cf27b503c5285c18
HaoochenLiang <101700394+HaoochenLiang@users.noreply.github.com>
2023-02-07 22:26:55 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -20,16 +20,12 @@ def main():
 	path = './test_imgs/'
 	Format='.JPG'
 
-	T=[]
 	for i in range(5):
 		index = i + 1
 		ue_path = path + 'u' + str(index) + Format
 		oe_path = path + 'o' + str(index) + Format
 
 		t=generate(oe_path, ue_path, MODEL_SAVE_PATH, index, output_path = './results/', format=Format)
-
-		T.append(t)
-		print("%s time: %s" % (index, t))
 
 if __name__ == '__main__':
 	main()
